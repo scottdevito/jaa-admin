0a8850a4ff04818feec30923730717e2e5628fbb
Scott DeVito <scottadevito@gmail.com>
2018-03-08 22:19:15 -0500

    Create AppContainer

---
+++ src/containers/app.C.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import App from '../App';
+
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = {};
+
+const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default AppContainer;
