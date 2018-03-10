b1010d44f6493268a61c28dc3558abf88abf66b0
Scott DeVito <scottadevito@gmail.com>
2018-03-09 21:59:04 -0500

    Add tabs to AdminConsole
    add react-swipeable

--- src/components/admin-console/admin_console.js
+++ src/components/admin-console/admin_console.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import AdminTabs from './admin_tabs.MUI';
 
-const AdminConsole = () => {
-  return <div>Admin Console</div>;
-};
+class AdminConsole extends Component {
+  render() {
+    return (
+      <AdminConsoleWrapper>
+        <AdminTabs />Admin Console
+      </AdminConsoleWrapper>
+    );
+  }
+}
 
 export default AdminConsole;
+
+const AdminConsoleWrapper = styled.div`
+  height: 35rem;
+  width: 35rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.25);
+
+  /* Styling for mobile screens */
+  @media (max-width: 425px) {
+    width: 85vw;
+    height: 26rem;
+  }
+
+  /* Styling for large screens */
+  @media (min-width: 768px) {
+    height: 25rem;
+    width: 45vw;
+  }
+`;
