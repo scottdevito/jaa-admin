edf8bd074bf441def42164be0e7a0ef33909b08e
Scott DeVito <scottadevito@gmail.com>
2018-03-13 16:46:06 -0400

    Create UTCDate field when a new episode is added

--- src/redux/actions/podcast.A.js
+++ src/redux/actions/podcast.A.js
@@ -3,11 +3,13 @@ import { ADD_EPISODE_SUCCESS, ADD_EPISODE_FAIL } from './types';
 
 const addNewEpisode = (title, date, url) => {
   return async dispatch => {
+    let UTCDateObj = new Date(date);
     try {
       const addEpisodeSuccess = await db.collection('podcastEpisodes').add({
         title,
         date,
         url,
+        UTCDate: UTCDateObj.getTime(),
       });
 
       dispatch({ type: ADD_EPISODE_SUCCESS, payload: addEpisodeSuccess });
